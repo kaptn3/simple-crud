a9374a8e9ccf5ea248fa72828752c31dd4860982
viktoriya <viktoriya@MacBook-Air-viktoriya.local>
2018-11-05 19:34:21 +0300

    Создание коллекции

--- app.js
+++ app.js
@@ -10,3 +10,40 @@ app.use(bodyParser.urlencoded({
 }));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+//  Schema
+let airplaneSchema = new mongoose.Schema({
+  gid: {
+    type: Number,
+    required: true
+  },
+  regNumber: {
+    type: Number,
+    default: 0
+  },
+  serialNumber: {
+    type: Number,
+    default: 0
+  },
+  manufacturer: {
+    type: String,
+    default: 'null'
+  },
+  type: {
+    type: String,
+    default: 'null'
+  },
+  date: {
+    type: Date,
+    default: null
+  },
+  airlines: {
+    type: String,
+    default: 'null'
+  },
+  status: {
+    type: String,
+    default: 'null'
+  }
+});
+let Airplane = mongoose.model('Airplane', airplaneSchema);
