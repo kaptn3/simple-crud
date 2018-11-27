b26f053c646ecda047baf79cff983b28caf64520
Viktoriya <kapitonenko.vick@ya.ru>
2018-11-28 00:29:04 +0300

    Проверка вводимых данных, обязательные поля

--- config/models.js
+++ config/models.js
@@ -3,31 +3,40 @@ var mongoose = require('mongoose');
 let airplaneSchema = new mongoose.Schema({
   regNumber: {
     type: String,
-    default: 0
+    default: 0,
+    required: true
   },
   serialNumber: {
     type: Number,
-    default: 0
+    default: 0,
+    min: 1,
+    max: 100,
+    required: true
   },
   manufacturer: {
     type: String,
-    default: 'none'
+    default: 'none',
+    required: true
   },
   type: {
     type: String,
-    default: 'none'
+    default: 'none',
+    required: true
   },
   date: {
     type: Date,
-    default: null
+    default: null,
+    required: true
   },
   airlines: {
     type: String,
-    default: 'none'
+    default: 'none',
+    required: true
   },
   status: {
     type: String,
-    default: 'none'
+    default: 'none',
+    required: true
   } 
 });
 
